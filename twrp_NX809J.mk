77a7b72fb3f3fe6cddd9fefbcc750395f4ab706a
Serendipity <150626035+ruoqing501@users.noreply.github.com>
2026-05-07 21:28:17 +0800

    Update twrp_NX809J.mk

--- twrp_NX809J.mk
+++ twrp_NX809J.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2025 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,14 +9,18 @@ DEVICE_PATH := device/nubia/NX809J
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-## Device identifier
-PRODUCT_DEVICE  := NX809J
-PRODUCT_NAME    := twrp_NX809J
-PRODUCT_BRAND   := nubia
+# Device identifier
+PRODUCT_DEVICE := NX809J
+PRODUCT_NAME := twrp_NX809J
+PRODUCT_BRAND := nubia
+PRODUCT_MANUFACTURER := nubia
+PRODUCT_MODEL := RedMagic 11 Pro +
+
+# Assert
 TARGET_OTA_ASSERT_DEVICE := NX809J
 
+# Fingerprint
+BUILD_FINGERPRINT := REDMAGIC/NX809J/NX809J:16/BQ2A.250705.001-BP2A.250605.031.A3/20260317.232707:user/release-keys
+
 # Theme
-TW_STATUS_ICONS_ALIGN   := center
-TW_CUSTOM_CLOCK_POS := 65
-TW_CUSTOM_CPU_POS := 240
-TW_CUSTOM_BATTERY_POS := 790
+TW_STATUS_ICONS_ALIGN := center
